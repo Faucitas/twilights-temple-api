3ae5be9d836280559a5d1520a83fe60d1d56f79c
Tommy Smith <tommy@tommyesmith.com>
2023-07-02 19:42:13 -0400

    Add ChapGPT endpoint

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response, session
 from flask_sock import Sock
 from polly import polly_request
 
+from oracle import ask_the_oracle
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'top-secret!'
+
 sock = Sock(app)
+
 
 @sock.route('/echo')
 def echo(sock):
@@ -12,3 +17,14 @@ def echo(sock):
         send_data = polly_request(data)
         sock.send(send_data)
 
+
+@app.route('/oracle', methods=['POST'])
+def the_oracle():
+    data = request.get_json()
+    message = data.get('message')
+    chat_log = session.get('chat_log')
+
+    answer, chat_log = ask_the_oracle(message, chat_log)
+    session['chat_log'] = chat_log
+
+    return jsonify({'answer': answer})
